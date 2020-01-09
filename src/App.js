4cf05246a4dee62e5ff0970e5fb68be5866a1598
geekwsie-jaime-lopez <geekwsie.jaime.lopez@gmail.com>
2020-01-09 14:26:16 -0800

    working react frontend

--- src/App.js
+++ src/App.js
@@ -1,144 +1,28 @@
 import React, { Component } from "react";
 import Modal from "./components/Modal";
 import axios from "axios";
+import { BrowserRouter as Router, Route, } from "react-router-dom";
+import Branch from './components/Branch';
+import Header from "./components/layout/header";
+import Login from "./components/accounts/login";
+import Register from "./components/accounts/register";
 
     class App extends Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          viewCompleted: false,
-          activeItem: {
-            title: "",
-            description: "",
-            completed: false
-          },
-          todoList: []
-        };
-      }
       componentDidMount() {
-        this.refreshList();
-      }
-      refreshList = () => {
-        axios
-          .get("https://django-react-drf.herokuapp.com/api/bank/")
-          .then(res => this.setState({ todoList: res.data }))
-          .catch(err => console.log(err));
+
       };
-      displayCompleted = status => {
-        if (status) {
-          return this.setState({ viewCompleted: true });
-        }
-        return this.setState({ viewCompleted: false });
-      };
-      renderTabList = () => {
-        return (
-          <div className="my-5 tab-list">
-            <span
-              onClick={() => this.displayCompleted(true)}
-              className={this.state.viewCompleted ? "active" : ""}
-            >
-              Bank
-            </span>
-            <span
-              onClick={() => this.displayCompleted(false)}
-              className={this.state.viewCompleted ? "" : "active"}
-            >
-              Accounts
-            </span>
-          </div>
-        );
-      };
-      renderItems = () => {
-        const { viewCompleted } = this.state;
-        const newItems = this.state.todoList.filter(
-          item => item.completed === viewCompleted
-        );
-        return newItems.map(item => (
-          <li
-            key={item.id}
-            className="list-group-item d-flex justify-content-between align-items-center"
-          >
-            <span
-              className={`todo-title mr-2 ${
-                this.state.viewCompleted ? "completed-todo" : ""
-              }`}
-              title={item.description}
-            >
-              {item.title}
-            </span>
-            <span>
-              <button
-                onClick={() => this.editItem(item)}
-                className="btn btn-secondary mr-2"
-              >
-                {" "}
-                Edit{" "}
-              </button>
-              <button
-                onClick={() => this.handleDelete(item)}
-                className="btn btn-danger"
-              >
-                Delete{" "}
-              </button>
-            </span>
-          </li>
-        ));
-      };
-      toggle = () => {
-        this.setState({ modal: !this.state.modal });
-      };
-      handleSubmit = item => {
-        this.toggle();
-        if (item.id) {
-          axios
-            .put(`https://django-react-drf.herokuapp.com/api/bank/${item.id}/`, item)
-            .then(res => this.refreshList());
-          return;
-        }
-        axios
-          .post("https://django-react-drf.herokuapp.com/api/bank/", item)
-          .then(res => this.refreshList());
-      };
-      handleDelete = item => {
-        axios
-          .delete(`https://django-react-drf.herokuapp.com/api/bank/${item.id}`)
-          .then(res => this.refreshList());
-      };
-      createItem = () => {
-        const item = { title: "", description: "", completed: false };
-        this.setState({ activeItem: item, modal: !this.state.modal });
-      };
-      editItem = item => {
-        this.setState({ activeItem: item, modal: !this.state.modal });
-      };
-      render() {
-        return (
-          <main className="content">
-            <h1 className="text-white text-uppercase text-center my-4">Bank App</h1>
-            <div className="row ">
-              <div className="col-md-6 col-sm-10 mx-auto p-0">
-                <div className="card p-3">
-                  <div className="">
-                    <button onClick={this.createItem} className="btn btn-primary">
-                      Add
-                    </button>
-                  </div>
-                  {this.renderTabList()}
-                  <ul className="list-group list-group-flush">
-                    {this.renderItems()}
-                  </ul>
-                </div>
-              </div>
-            </div>
-            {this.state.modal ? (
-              <Modal
-                activeItem={this.state.activeItem}
-                toggle={this.toggle}
-                onSave={this.handleSubmit}
-              />
-            ) : null}
-          </main>
-        );
+
+      render(){
+        return(
+        <Router>
+            <Header/>
+          <Route exact path="/branch" component={Branch}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/register" component={Register}/>
+
+        </Router>
+      );
       }
     }
+
     export default App;
