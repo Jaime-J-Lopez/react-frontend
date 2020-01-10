4eec6398bdf29f65c985be44c17e5285e2c34af7
geekwsie-jaime-lopez <geekwsie.jaime.lopez@gmail.com>
2020-01-10 15:21:23 -0800

    working branch login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from "react";
 import Modal from "./components/Modal";
 import axios from "axios";
-import { BrowserRouter as Router, Route, } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Branch from './components/Branch';
 import Header from "./components/layout/header";
 import Login from "./components/accounts/login";
 import Register from "./components/accounts/register";
+import PrivateRoute from "./components/common/PrivateRoute";
+import { Provider } from 'react-redux';
+import store from "./store";
+import { loadUser } from './actions/auth';
 
     class App extends Component {
       componentDidMount() {
-
+      store.dispatch(loadUser());
       };
 
       render(){
         return(
+        <Provider store={store}>
         <Router>
-            <Header/>
-          <Route exact path="/branch" component={Branch}/>
+          <Header/>
+          <Switch>
+          <PrivateRoute exact path="/branch" component={Branch}/>
           <Route exact path="/login" component={Login}/>
           <Route exact path="/register" component={Register}/>
-
+          </Switch>
         </Router>
+        </Provider>
       );
       }
     }
